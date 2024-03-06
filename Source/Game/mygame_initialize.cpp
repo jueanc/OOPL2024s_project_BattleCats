7d8174190c690c882304d3b85b7c6f2bcc7226bf
jueanc <jueanc@gmail.com>
2024-03-06 23:04:15 +0800

    lion

--- Source/Game/mygame_initialize.cpp
+++ Source/Game/mygame_initialize.cpp
@@ -23,13 +23,13 @@ void CGameStateInit::OnInit()
 	// 當圖很多時，OnInit載入所有的圖要花很多時間。為避免玩遊戲的人
 	//     等的不耐煩，遊戲會出現「Loading ...」，顯示Loading的進度。
 	//
-	ShowInitProgress(0, "Start Initialize...");	// 一開始的loading進度為0%
+	ShowInitProgress(0, "Start Catting...");	// 一開始的loading進度為0%
 
 	load_background();
 	//
 	// 開始載入資料
 	//
-	//Sleep(1000);				// 放慢，以便看清楚進度，實際遊戲請刪除此Sleep
+	Sleep(1000);				// 放慢，以便看清楚進度，實際遊戲請刪除此Sleep
 	//
 	// 此OnInit動作會接到CGameStaterRun::OnInit()，所以進度還沒到100%
 	//
@@ -52,9 +52,29 @@ void CGameStateInit::OnLButtonDown(UINT nFlags, CPoint point)
 void CGameStateInit::OnShow()
 {
 	background.ShowBitmap();
+	draw_text();
 }
 
 void CGameStateInit::load_background() {
 	background.LoadBitmapByString({ "resources/main.bmp" });
 	background.SetTopLeft(0, 0);
+}
+
+void CGameStateInit::draw_text() {
+
+
+	/*
+	CDC *pDC = CDDraw::GetBackCDC();
+
+	//* Print title 
+	CTextDraw::ChangeFontLog(pDC, 36, "微軟正黑體", RGB(255, 255, 255));
+	CTextDraw::Print(pDC, 79, 228, "Game Framework Practice");
+
+	//* Print info 
+	CTextDraw::ChangeFontLog(pDC, 24, "微軟正黑體", RGB(255, 255, 255));
+	CTextDraw::Print(pDC, 182, 431, "Press any key to start");
+
+	CDDraw::ReleaseBackCDC();	
+	*/
+
 }
