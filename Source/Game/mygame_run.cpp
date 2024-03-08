a40f4127ab813287a3d5b642b3b8005bbc60b521
jueanc <jueanc@gmail.com>
2024-03-08 21:46:13 +0800

    push
    ．修正一些小錯誤
    ．刪除多餘的檔案

--- Source/Game/mygame_run.cpp
+++ Source/Game/mygame_run.cpp
@@ -52,7 +52,6 @@ void CGameStateRun::OnInit()  								// 遊戲的初值及圖形設定
 		});
 	background.SetTopLeft(0, 0);	
 
-
 	catButton.LoadBitmapByString({
 	"resources/pic/Cat_text.bmp",
 	"resources/pic/Cat_text_dark.bmp"
@@ -79,20 +78,17 @@ void CGameStateRun::OnKeyUp(UINT nChar, UINT nRepCnt, UINT nFlags)
 
 void CGameStateRun::OnLButtonDown(UINT nFlags, CPoint point)  // 處理滑鼠的動作
 {
-	
+
 	if ((point.x >= 171 && point.x <= 302) && (point.y >=580&& point.y <= 680) && (catButton.GetFrameIndexOfBitmap() == 0)) {
 		catButtonTime = time(0);
 		catButton.SetFrameIndexOfBitmap(1);
 	}
 
 
-
-
 	if ((point.x >= 322 && point.x <= 453) && (point.y >= 580 && point.y <= 680) && (Tank_catButton.GetFrameIndexOfBitmap() == 0)) {
 		Tank_catButtonTime = time(0);
 		Tank_catButton.SetFrameIndexOfBitmap(1);
 	}
-
 }
 
 void CGameStateRun::OnLButtonUp(UINT nFlags, CPoint point)	// 處理滑鼠的動作
